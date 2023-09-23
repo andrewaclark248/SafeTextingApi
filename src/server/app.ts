7615d09a9f14a8b6765ff89056b137d8cd3df212
andrewaclark248 <andrewaclark248@gmail.com>
2023-09-23 17:17:39 -0400

    api scaffolding

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,8 +1,10 @@
 
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+import { apiDocumentation } from './../docs/apidoc';
 
 const app = express()
-const PORT = process.env.PORT || 3000;
+const PORT = 5000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -11,3 +13,5 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`)
 })
+
+app.use('/documentation', swaggerUi.serve, swaggerUi.setup(apiDocumentation));
