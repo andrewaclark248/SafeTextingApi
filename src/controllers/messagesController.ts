d32d4c5f6dca397d2ae115aff1ad263b3c33bc94
andrewaclark248 <andrewaclark248@gmail.com>
2024-02-24 21:51:10 -0500

    send message

--- src/controllers/messagesController.ts
+++ src/controllers/messagesController.ts
@@ -4,7 +4,7 @@ import { RequestContext } from "@mikro-orm/core";
 import { Group } from '../models/Group'
 import { User } from '../models/User'
 import { Message } from '../models/Message'
-import { Collection } from "@mikro-orm/core";
+import twilio from "twilio";
 
 
 /**
@@ -23,29 +23,47 @@ export async function create(req: Request, res: Response) {
     const user = await RequestContext.getEntityManager()?.findOne(User, { email: email })
 
     //find groups
-    const groups: Group[] | undefined = await RequestContext.getEntityManager()?.find(Group, { name: selectedGroups })
+    const groups: Group[] | undefined = await RequestContext.getEntityManager()?.find(Group, { name: selectedGroups }, { populate: ['people'] })
     
     //create message record
     if (groups != undefined && user != undefined) {
-        let newMessage = new Message();
-        newMessage.name = name;
-        newMessage.groups.add(groups);
-        newMessage.messageType = messageType;
-        newMessage.message = message;
-        newMessage.user = user;
+        //let newMessage = new Message();
+        //newMessage.name = name;
+        //newMessage.groups.add(groups);
+        //newMessage.messageType = messageType;
+        //newMessage.message = message;
+        //newMessage.user = user;
 
-        const persistMessage  = await RequestContext.getEntityManager()?.create(Message, newMessage);
+        //const persistMessage  = await RequestContext.getEntityManager()?.create(Message, newMessage);
     
-        await RequestContext.getEntityManager()?.persistAndFlush(persistMessage!);
+        //await RequestContext.getEntityManager()?.persistAndFlush(persistMessage!);
+
+        sendMessage(groups);
     
         res.status(200).json({success: true, msg: "Successfully created message"})
     } else {
 
     }
 
+}
 
 
-    
+const sendMessage = async (groups: Group[]) => {
+    let people = groups[0].people;
 
+    console.log("people = ", people)
+
+    /***
+    const accountSid = process.env.ACCOUNT_SID;
+    const authToken = process.env.AUTH_TOKEN;
+    const client = twilio(accountSid, authToken);
+
+    await client.messages
+        .create({
+            body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
+            from: '+15017122661',
+            to: '+15558675310'
+        })
+         */
 
 }
